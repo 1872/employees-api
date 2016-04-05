ce06672673be74fb64a67d76134914db4290925c
Peter Jang <peter.x.jang@gmail.com>
2016-04-04 20:14:05 -0500

    added employee api create action

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -8,4 +8,13 @@ class EmployeesController < ApplicationController
     @employee = Employee.find_by(id: params[:id])
     render "show.json.jbuilder"
   end
+
+  def create
+    @employee = Employee.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email]
+    )
+    render "show.json.jbuilder"
+  end
 end
