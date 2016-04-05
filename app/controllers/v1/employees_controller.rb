94e032c5ac3686afe20d2538633d3c90ff54d455
Peter Jang <peter.x.jang@gmail.com>
2016-04-05 18:29:22 -0500

    namespaced employees api under v1 namespace

--- app/controllers/v1/employees_controller.rb
+++ app/controllers/v1/employees_controller.rb
@@ -1,4 +1,4 @@
-class EmployeesController < ApplicationController
+class V1::EmployeesController < ApplicationController
   def index
     @employees = Employee.all
     render "index.json.jbuilder"
@@ -25,11 +25,11 @@ class EmployeesController < ApplicationController
   def update
     @employee = Employee.find_by(id: params[:id])
     @employee.update(
-      first_name: params[:first_name],
-      last_name: params[:last_name],
-      email: params[:email],
-      birthdate: params[:birthdate],
-      ssn: params[:ssn]
+      first_name: params[:first_name] || @employee.first_name,
+      last_name: params[:last_name] || @employee.last_name,
+      email: params[:email] || @employee.email,
+      birthdate: params[:birthdate] || @employee.birthdate,
+      ssn: params[:ssn] || @employee.ssn
     )
     render "show.json.jbuilder"
   end
